0d70156535267a9197e3fca1af49f82245067e0b
Paula Origlio <minplynn@aol.com>
2015-09-28 17:26:00 -0600

    added create world and create user from sign up form

--- script.js
+++ script.js
@@ -2,10 +2,113 @@ angular.module("characterApp", [])
 
 angular.module("characterApp").controller("characterController",["$scope",function($scope){
 
+
+
+	// USER REGISTRATION
+
+	users = []
+
+	var User = function (username,password) {
+		this.username  = username
+		this.password  = password
+	}
+
+	var register = function ( ) {
+		
+		var newUser = new User($scope.newUsername,$scope.newPassword)
+		users.push(newUser)
+		$scope.newUsername = ""
+		$scope.newPassword = ""
+	}
+
+	// END USER REGISTRATION
+
+	// LOGIN VALIDATION
+
+	var validate = function ( ) {
+
+		var valid = users.filter(function(element){
+			if(($scope.loginUsername === element.username)&&($scope.loginPassword === element.password)){
+				return true
+			}
+		})
+
+		$scope.currentUser = valid[0].username
+		return $scope.currentUser
+
+	}
+
+	// END LOGIN VALIDATION
+
+
+	// CREATE WORLD
+
+	worlds = []
+
+	var World = function (worldName,worldInfo) {
+		this.worldName = worldName
+		this.worldInfo = worldInfo
+	}
+
+
+	var makeWorld = function ( ) {
+		var world = new World($scope.worldName,$scope.worldInfo)
+		worlds.push(world)
+		World.prototype.type = "world"
+		World.prototype.username = $scope.newUsername || $scope.currentUser
+		console.log(worlds)
+	}
+
+
+	// END CREATE WORLD
+
+
+
+
+
+
+
+	// BUILT-IN TEMPLATES
+
+	// END BUILT IN TEMPLATES
+
+
+
+
+
+
+
+
+
+	// CLICK AND HIDE/SHOW EVENTS
+
+	$scope.logout = function ( ) {
+
+		$scope.introHide = false;
+		$scope.userShow = false;
+		$scope.registerShow = false;
+		$scope.sheetShow = false;
+		$scope.characterShow = false;
+		$scope.createTemplateShow = false;
+		$scope.createWorldShow = false;
+		$scope.createCharShow = false;
+		$scope.createSheetShow = false;
+		$scope.editSheetShow = false;
+
+	}
+
 	$scope.getStarted = function ( ) {
 
 		$scope.introHide = true;
 		$scope.registerShow = true;
+		$scope.userShow = false;
+		$scope.sheetShow = false;
+		$scope.characterShow = false;
+		$scope.createTemplateShow = false;
+		$scope.createWorldShow = false;
+		$scope.createCharShow = false;
+		$scope.createSheetShow = false;
+		$scope.editSheetShow = false;
 
 	}
 
@@ -21,6 +124,7 @@ angular.module("characterApp").controller("characterController",["$scope",functi
 		$scope.createCharShow = false;
 		$scope.createSheetShow = false;
 		$scope.editSheetShow = false;
+		validate()
 
 	}
 
@@ -28,7 +132,8 @@ angular.module("characterApp").controller("characterController",["$scope",functi
 
 		$scope.registerShow = false;
 		$scope.userShow = true;
-
+		register()
+		makeWorld()
 	}
 
 	$scope.newWorld = function ( ) {
@@ -38,6 +143,7 @@ angular.module("characterApp").controller("characterController",["$scope",functi
 		$scope.createCharShow = false;
 		$scope.characterShow = false;
 		$scope.createSheetShow = false;
+		$scope.sheetShow = false;
 
 	}
 
@@ -48,6 +154,7 @@ angular.module("characterApp").controller("characterController",["$scope",functi
 		$scope.createCharShow = false;
 		$scope.characterShow = false;
 		$scope.createSheetShow = false;
+		$scope.sheetShow = false;
 
 	}
 
@@ -58,6 +165,7 @@ angular.module("characterApp").controller("characterController",["$scope",functi
 		$scope.createWorldShow = false;
 		$scope.characterShow = false;
 		$scope.createSheetShow = false;
+		$scope.sheetShow = false;
 
 	}
 
@@ -91,6 +199,7 @@ angular.module("characterApp").controller("characterController",["$scope",functi
 	$scope.createSheet = function ( ) {
 
 		$scope.createSheetShow = true;
+		$scope.sheetShow = false;
 
 	}
 
@@ -111,5 +220,22 @@ angular.module("characterApp").controller("characterController",["$scope",functi
 		$scope.sheetShow = false;
 
 	}
+
+	$scope.closeInnerForm = function ( ) {
+
+		$scope.createSheetShow = false;
+		$scope.sheetShow = false;
+
+	}
+
+	$scope.openCharSheet = function ( ) {
+
+		$scope.sheetShow = true;
+
+	}
+
+	// END CLICK AND HIDE/SHOW EVENTS
+
+
 
 }])
