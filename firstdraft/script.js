4dc2e0e5c43a67536da05f45570b47cc6d909490
Paula Origlio <minplynn@aol.com>
2015-10-08 15:45:47 -0600

    re-organized folders

---
+++ firstdraft/script.js
@@ -0,0 +1,307 @@
+angular.module("characterApp", [])
+
+angular.module("characterApp").controller("characterController",["$scope",function($scope){
+
+
+
+	// USER REGISTRATION
+
+	users = []
+
+	var User = function (username,password) {
+		this.username  = username
+		this.password  = password
+	}
+
+	var register = function ( ) {
+		
+		var newUser = new User($scope.newUsername,$scope.newPassword)
+		users.push(newUser)
+		$scope.newUsername = ""
+		$scope.newPassword = ""
+	}
+
+	// END USER REGISTRATION
+
+	// LOGIN VALIDATION
+
+	var validate = function ( ) {
+
+		var valid = users.filter(function(element){
+			if(($scope.loginUsername === element.username)&&($scope.loginPassword === element.password)){
+				return true
+			}
+		})
+
+		$scope.currentUser = valid[0].username
+		World.prototype.username = $scope.newUsername || $scope.currentUser
+		return $scope.currentUser
+	}
+
+	// END LOGIN VALIDATION
+
+
+	// CREATE WORLD
+
+	$scope.worlds = []
+
+	var World = function (worldName,worldInfo) {
+		this.worldName = worldName
+		this.worldInfo = worldInfo
+	}
+
+
+	var makeWorld = function ( ) {
+		var world = new World($scope.worldName,$scope.worldInfo)
+		$scope.worlds.push(world)
+		World.prototype.type = "world"
+	}
+
+	// END CREATE WORLD
+
+
+	// CREATE CHARACTER
+
+	$scope.characters = []
+
+	var Character = function (characterName,characterInfo) {
+		this.characterName = characterName
+		this.characterInfo = characterInfo
+	}
+
+
+	var makeCharacter = function ( ) {
+		var character = new Character($scope.characterName,$scope.characterInfo)
+		Character.prototype.type = "character"
+		character.world = $scope.worldIndex
+		$scope.characters.push(character)
+		return character
+	}
+
+	// END CREATE CHARACTER
+
+
+
+
+	// CREATE TEMPLATE
+
+	$scope.userTemplates = []
+
+	var UserTemplate = function (sheetName,sheetInfo,numberFields,fieldsArray) {
+		this.sheetName = sheetName
+		this.sheetInfo = sheetInfo
+		this.numberFields = numberFields
+		this.fieldsArray = this.fieldsArray()
+	}
+
+
+	var makeUserTemplate = function ( ) {
+		var userTemplate = new UserTemplate($scope.sheetName,$scope.sheetInfo,$scope.numberFields)
+		UserTemplate.prototype.type = "template"
+		UserTemplate.prototype.username = $scope.currentUser
+		$scope.userTemplates.push(userTemplate)
+		return userTemplate
+	}
+
+	UserTemplate.prototype.fieldsArray = function(template){
+		var fieldsArray = []
+		for(var x = 0; x < this.numberFields; x++){
+			fieldsArray.push({question : "question", answer : "answer"})
+		}
+		console.log(fieldsArray)
+		return fieldsArray
+	}
+
+	var editUserTemplate = function ( ) {
+		$scope.showNewTemplateFields = true;
+	}
+
+	// END CREATE TEMPLATE
+
+
+
+
+
+
+
+
+
+	// CLICK AND HIDE/SHOW EVENTS
+
+	$scope.logout = function ( ) {
+
+		$scope.introHide = false;
+		$scope.userShow = false;
+		$scope.registerShow = false;
+		$scope.sheetShow = false;
+		$scope.characterShow = false;
+		$scope.createTemplateShow = false;
+		$scope.createWorldShow = false;
+		$scope.createCharShow = false;
+		$scope.createSheetShow = false;
+		$scope.editSheetShow = false;
+
+	}
+
+	$scope.getStarted = function ( ) {
+
+		$scope.introHide = true;
+		$scope.registerShow = true;
+		$scope.userShow = false;
+		$scope.sheetShow = false;
+		$scope.characterShow = false;
+		$scope.createTemplateShow = false;
+		$scope.createWorldShow = false;
+		$scope.createCharShow = false;
+		$scope.createSheetShow = false;
+		$scope.editSheetShow = false;
+
+	}
+
+	$scope.login = function ( ) {
+
+		$scope.userShow = true;
+		$scope.introHide = true;
+		$scope.registerShow = false;
+		$scope.sheetShow = false;
+		$scope.characterShow = false;
+		$scope.createTemplateShow = false;
+		$scope.createWorldShow = false;
+		$scope.createCharShow = false;
+		$scope.createSheetShow = false;
+		$scope.editSheetShow = false;
+		$scope.loginPromptShow = false;
+		validate()
+
+	}
+
+	$scope.newUser = function ( ) {
+
+		$scope.registerShow = false;
+		$scope.loginPromptShow = true;
+		register()
+		makeWorld()
+	}
+
+	$scope.newWorld = function ( ) {
+
+		$scope.createWorldShow = true;
+		$scope.createTemplateShow = false;
+		$scope.createCharShow = false;
+		$scope.characterShow = false;
+		$scope.createSheetShow = false;
+		$scope.sheetShow = false;
+		$scope.worldName = ""
+		$scope.worldInfo = ""
+	}
+
+	$scope.newTemplate = function ( ) {
+
+		$scope.createTemplateShow = true;
+		$scope.createWorldShow = false;
+		$scope.createCharShow = false;
+		$scope.characterShow = false;
+		$scope.createSheetShow = false;
+		$scope.sheetShow = false;
+
+	}
+
+	$scope.newCharacter = function ($index) {
+
+		$scope.createCharShow = true;
+		$scope.createTemplateShow = false;
+		$scope.createWorldShow = false;
+		$scope.characterShow = false;
+		$scope.createSheetShow = false;
+		$scope.sheetShow = false;
+		$scope.worldIndex = $index.worldName
+	}
+
+	$scope.submitWorld = function ( ) {
+
+
+		$scope.createWorldShow = false;
+		makeWorld()
+
+	}
+
+	$scope.submitTemplate = function ( ) {
+
+		$scope.createTemplateShow = false;
+		makeUserTemplate()
+		editUserTemplate()
+
+	}
+
+	$scope.submitCharacter = function ( ) {
+
+		$scope.createCharShow = false;
+		makeCharacter()
+	}
+
+	$scope.viewCharacter = function ( ) {
+
+		$scope.characterShow = true;
+		$scope.createCharShow = false;
+		$scope.createTemplateShow = false;
+		$scope.createWorldShow = false;
+
+	}
+
+	$scope.createSheet = function ( ) {
+
+		$scope.createSheetShow = true;
+		$scope.sheetShow = false;
+
+	}
+
+	$scope.submitSheet = function ( ) {
+
+		$scope.createSheetShow = false;
+
+	}
+
+	$scope.close = function ( ) {
+
+		$scope.characterShow = false;
+		$scope.createCharShow = false;
+		$scope.createTemplateShow = false;
+		$scope.createWorldShow = false;
+		$scope.createSheetShow = false;
+		$scope.editSheetShow = false;
+		$scope.sheetShow = false;
+
+	}
+
+	$scope.closeInnerForm = function ( ) {
+
+		$scope.createSheetShow = false;
+		$scope.sheetShow = false;
+
+	}
+
+	$scope.openCharSheet = function ( ) {
+
+		$scope.sheetShow = true;
+
+	}
+
+	// END CLICK AND HIDE/SHOW EVENTS
+
+
+
+}])
+
+// THIS FILTER EXPECTS AN ARRAY OF CHARACTERS AND A WORLD NAME
+
+angular.module("characterApp").filter('filterbyselected', function(){
+  return function(input,worldName){
+    var output = [];
+    angular.forEach(input, function(character){
+      if(character.world === worldName){
+        output.push(character)
+      }
+    })
+    return output;
+  }
+})
--- script.js
+++ script.js
@@ -1,307 +1,155 @@
 angular.module("characterApp", [])
 
-angular.module("characterApp").controller("characterController",["$scope",function($scope){
+angular.module("characterApp").controller("characterController",["$scope","characterFactory", function($scope,characterFactory){
 
+	// SUBMIT NEW CHARACTER
 
-
-	// USER REGISTRATION
-
-	users = []
-
-	var User = function (username,password) {
-		this.username  = username
-		this.password  = password
+	$scope.charSubmit = function ( ) {
+		addChar( )
 	}
 
-	var register = function ( ) {
-		
-		var newUser = new User($scope.newUsername,$scope.newPassword)
-		users.push(newUser)
-		$scope.newUsername = ""
-		$scope.newPassword = ""
+	// VIEW SAVED CHAR SHEETs
+
+	$scope.selectCharacter= function ($index) {
+
+		deactivateTemplates()
+		deactivateChars()
+		$scope.characters[$index].active = true
+
 	}
 
-	// END USER REGISTRATION
 
-	// LOGIN VALIDATION
+	// ADD NEW CHAR SHEET
 
-	var validate = function ( ) {
+	$scope.addSheet = function ($index) {
 
-		var valid = users.filter(function(element){
-			if(($scope.loginUsername === element.username)&&($scope.loginPassword === element.password)){
-				return true
+		deactivateChars()
+		$scope.templateButtons = true
+		$scope.characters[$index].active = true
+	}
+
+	$scope.selectTemplate = function($index){
+		deactivateTemplates()
+		$scope.templates[$index].active = true
+	}
+
+	// SAVE CHARACTER SHEET
+
+	$scope.saveSheet = function($index){
+		var newSheet = angular.copy($scope.templates[$index])
+		$scope.templateButtons = false
+		$scope.characters[findCharacterIndex()].charSheets.push(newSheet)
+		deactivateTemplates()
+		deactivateChars()
+		$scope.templateButtons = true
+		console.log($scope.templates[$index])
+		$scope.clearForm($index)
+	}
+
+	$scope.clearForm = function($index){
+		for(var x = 0; x<$scope.templates[$index].fieldsArray.length;x++){
+			$scope.templates[$index].fieldsArray[x].answer = ""
+		}
+	}
+
+	// -=-=-=-=-=-=-=-=-=--=-=-=-=-=--=-=
+	// IGNORE EVERYTHING BELOW THIS LINE
+	// -=-=-=-=-=-=-=-=-=--=-=-=-=-=--=-=
+
+	// HIDES / SHOWS
+
+
+
+	// FUNCTIONS
+
+	var findCharacterIndex = function(){
+
+		var charIndex = ""
+		$scope.characters.forEach(function(element,index){
+			if(element.active === true){
+				charIndex = index
 			}
 		})
-
-		$scope.currentUser = valid[0].username
-		World.prototype.username = $scope.newUsername || $scope.currentUser
-		return $scope.currentUser
+		return charIndex
 	}
 
-	// END LOGIN VALIDATION
-
-
-	// CREATE WORLD
-
-	$scope.worlds = []
-
-	var World = function (worldName,worldInfo) {
-		this.worldName = worldName
-		this.worldInfo = worldInfo
-	}
-
-
-	var makeWorld = function ( ) {
-		var world = new World($scope.worldName,$scope.worldInfo)
-		$scope.worlds.push(world)
-		World.prototype.type = "world"
-	}
-
-	// END CREATE WORLD
-
-
-	// CREATE CHARACTER
-
-	$scope.characters = []
-
-	var Character = function (characterName,characterInfo) {
-		this.characterName = characterName
-		this.characterInfo = characterInfo
-	}
-
-
-	var makeCharacter = function ( ) {
-		var character = new Character($scope.characterName,$scope.characterInfo)
-		Character.prototype.type = "character"
-		character.world = $scope.worldIndex
-		$scope.characters.push(character)
-		return character
-	}
-
-	// END CREATE CHARACTER
-
-
-
-
-	// CREATE TEMPLATE
-
-	$scope.userTemplates = []
-
-	var UserTemplate = function (sheetName,sheetInfo,numberFields,fieldsArray) {
-		this.sheetName = sheetName
-		this.sheetInfo = sheetInfo
-		this.numberFields = numberFields
-		this.fieldsArray = this.fieldsArray()
-	}
-
-
-	var makeUserTemplate = function ( ) {
-		var userTemplate = new UserTemplate($scope.sheetName,$scope.sheetInfo,$scope.numberFields)
-		UserTemplate.prototype.type = "template"
-		UserTemplate.prototype.username = $scope.currentUser
-		$scope.userTemplates.push(userTemplate)
-		return userTemplate
-	}
-
-	UserTemplate.prototype.fieldsArray = function(template){
-		var fieldsArray = []
-		for(var x = 0; x < this.numberFields; x++){
-			fieldsArray.push({question : "question", answer : "answer"})
+	addChar = function ( ) {
+		if($scope.charName !== undefined ){
+			var Character = function (charName) {
+				this.charName = charName
+				this.charSheets = []
+				this.active = false
+			}
+			var character = new Character($scope.charName)
+			$scope.characters.push(character)
 		}
-		console.log(fieldsArray)
-		return fieldsArray
+		$scope.charName = undefined
 	}
 
-	var editUserTemplate = function ( ) {
-		$scope.showNewTemplateFields = true;
+	var deactivateChars = function () {
+		$scope.characters.forEach(function(element){
+			element.active = false
+		})
 	}
 
-	// END CREATE TEMPLATE
-
-
-
-
-
-
-
-
-
-	// CLICK AND HIDE/SHOW EVENTS
-
-	$scope.logout = function ( ) {
-
-		$scope.introHide = false;
-		$scope.userShow = false;
-		$scope.registerShow = false;
-		$scope.sheetShow = false;
-		$scope.characterShow = false;
-		$scope.createTemplateShow = false;
-		$scope.createWorldShow = false;
-		$scope.createCharShow = false;
-		$scope.createSheetShow = false;
-		$scope.editSheetShow = false;
-
+	var deactivateTemplates = function (){
+		$scope.templates.forEach(function(element){
+			element.active = false
+		})
 	}
 
-	$scope.getStarted = function ( ) {
+	// PRESET CHARACTERS
 
-		$scope.introHide = true;
-		$scope.registerShow = true;
-		$scope.userShow = false;
-		$scope.sheetShow = false;
-		$scope.characterShow = false;
-		$scope.createTemplateShow = false;
-		$scope.createWorldShow = false;
-		$scope.createCharShow = false;
-		$scope.createSheetShow = false;
-		$scope.editSheetShow = false;
+	$scope.characters = [
 
+		{
+			charName : "Lacey",
+			charSheets : [],
+			active: false
+		},
+		{
+			charName : "Mo",
+			charSheets : [],
+			active: false
+		},
+			{charName : "Will",
+			charSheets : [],
+			active: false
+		},
+		{
+			charName : "Jack",
+			charSheets : [],
+			active: false
+		},
+		{
+			charName : "Lucinda",
+			charSheets : [],
+			active: false
+		},
+		{
+			charName : "Carmen",
+			charSheets : [],
+			active: false
+		}
+			
+			]
+
+	// PRESET REF SHEETS
+
+	$scope.templates = []
+
+	var Ref = function (sheetName,fieldsArray){
+		this.sheetName = sheetName
+		this.fieldsArray = fieldsArray
+		this.active = false
+		$scope.templates.push(this)
 	}
 
-	$scope.login = function ( ) {
-
-		$scope.userShow = true;
-		$scope.introHide = true;
-		$scope.registerShow = false;
-		$scope.sheetShow = false;
-		$scope.characterShow = false;
-		$scope.createTemplateShow = false;
-		$scope.createWorldShow = false;
-		$scope.createCharShow = false;
-		$scope.createSheetShow = false;
-		$scope.editSheetShow = false;
-		$scope.loginPromptShow = false;
-		validate()
-
-	}
-
-	$scope.newUser = function ( ) {
-
-		$scope.registerShow = false;
-		$scope.loginPromptShow = true;
-		register()
-		makeWorld()
-	}
-
-	$scope.newWorld = function ( ) {
-
-		$scope.createWorldShow = true;
-		$scope.createTemplateShow = false;
-		$scope.createCharShow = false;
-		$scope.characterShow = false;
-		$scope.createSheetShow = false;
-		$scope.sheetShow = false;
-		$scope.worldName = ""
-		$scope.worldInfo = ""
-	}
-
-	$scope.newTemplate = function ( ) {
-
-		$scope.createTemplateShow = true;
-		$scope.createWorldShow = false;
-		$scope.createCharShow = false;
-		$scope.characterShow = false;
-		$scope.createSheetShow = false;
-		$scope.sheetShow = false;
-
-	}
-
-	$scope.newCharacter = function ($index) {
-
-		$scope.createCharShow = true;
-		$scope.createTemplateShow = false;
-		$scope.createWorldShow = false;
-		$scope.characterShow = false;
-		$scope.createSheetShow = false;
-		$scope.sheetShow = false;
-		$scope.worldIndex = $index.worldName
-	}
-
-	$scope.submitWorld = function ( ) {
-
-
-		$scope.createWorldShow = false;
-		makeWorld()
-
-	}
-
-	$scope.submitTemplate = function ( ) {
-
-		$scope.createTemplateShow = false;
-		makeUserTemplate()
-		editUserTemplate()
-
-	}
-
-	$scope.submitCharacter = function ( ) {
-
-		$scope.createCharShow = false;
-		makeCharacter()
-	}
-
-	$scope.viewCharacter = function ( ) {
-
-		$scope.characterShow = true;
-		$scope.createCharShow = false;
-		$scope.createTemplateShow = false;
-		$scope.createWorldShow = false;
-
-	}
-
-	$scope.createSheet = function ( ) {
-
-		$scope.createSheetShow = true;
-		$scope.sheetShow = false;
-
-	}
-
-	$scope.submitSheet = function ( ) {
-
-		$scope.createSheetShow = false;
-
-	}
-
-	$scope.close = function ( ) {
-
-		$scope.characterShow = false;
-		$scope.createCharShow = false;
-		$scope.createTemplateShow = false;
-		$scope.createWorldShow = false;
-		$scope.createSheetShow = false;
-		$scope.editSheetShow = false;
-		$scope.sheetShow = false;
-
-	}
-
-	$scope.closeInnerForm = function ( ) {
-
-		$scope.createSheetShow = false;
-		$scope.sheetShow = false;
-
-	}
-
-	$scope.openCharSheet = function ( ) {
-
-		$scope.sheetShow = true;
-
-	}
-
-	// END CLICK AND HIDE/SHOW EVENTS
-
-
-
+	var ref1 = new Ref("Basic Info",[{question: "Gender"},{question: "Orientation"},{question: "Species"},{question: "Breed"},{question: "Hobbies"},{question: "Enneagram Type"},{question: "Job"},{question: "Car"},{question: "Location"},{question: "Birthday"},{question: "Birthplace"}])
+	var ref2 = new Ref("Physical Characteristics",[{question: "Height"},{question: "Hairstyle"},{question: "Fur Color"},{question: "Hair Color"},{question: "Weight"},{question: "Piercings"},{question: "Tattoos"},{question: "Scars"},{question: "Eye Color"},{question: "Clothing Style"},{question: "Build"}])
+	var ref3 = new Ref("Family and Friends",[{question: "best friend"},{question: "ex-friend"},{question: "frienemy"},{question: "deceased friends"},{question: "deceased relatives"},{question: "the one s/he dreads seeing on holidays"},{question: "siblings"},{question: "parents"},{question: "step-family members"},{question: "childhood best friend"},{question: "the one s/he'd have a crush on if they weren't family"}])
+	var ref4 = new Ref("Romantic Past, Present, and Future",[{question: "sexual orientation"},{question: "current crush"},{question: "first lover"},{question: "first crush"},{question: "monogamy versus polyamory?"},{question: "celebrity crush"},{question: "first kiss"},{question: "marriage?"},{question: "children?"},{question: "the one that got away"},{question: "worst breakup"}])
+	var ref5 = new Ref("Probing Questions",[{question: "Would s/he give his/her life for a stranger?"},{question: "What does s/he feel most guilty about from the past?"},{question: "What is s/he ashamed of?"},{question: "What is his/her opinion of drugs?"},{question: "What is a dream s/he longs for but keeps secret?"},{question: "Would s/he ever cheat on his/her partner?"},{question: "What is the worst thing s/he'd do for money?"},{question: "How has s/he been betrayed?"},{question: "What is his/her favorite childhood memory?"},{question: "What is his/her most awful childhood memory?"},{question: "What is his/her best character trait?"}])
+	var ref6 = new Ref("Favorites",[{question: "food"},{question: "color"},{question: "restaurant"},{question: "movie"},{question: "song"},{question: "family member"},{question: "sport"},{question: "item of clothing"},{question: "dessert"},{question: "season"},{question: "animal"}])
+	var ref7 = new Ref("Coder Profile",[{question: "former career"},{question:"dream job"},{question:"favorite framework"},{question:"favorite snack"},{question:"most hated browser"},{question:"favorite CSS color"},{question:"front-end or back-end?"},{question:"favorite website"},{question:"favorite beer"},{question:"favorite coffee shop"},{question:"catch phrase"}])
 }])
-
-// THIS FILTER EXPECTS AN ARRAY OF CHARACTERS AND A WORLD NAME
-
-angular.module("characterApp").filter('filterbyselected', function(){
-  return function(input,worldName){
-    var output = [];
-    angular.forEach(input, function(character){
-      if(character.world === worldName){
-        output.push(character)
-      }
-    })
-    return output;
-  }
-})
