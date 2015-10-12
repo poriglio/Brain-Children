06b42454b7fd3fe6f26dac325ca9606377c5580d
Paula Origlio <minplynn@aol.com>
2015-10-11 18:25:15 -0600

    ...

--- app.js
+++ app.js
@@ -1,27 +1,26 @@
 // REQUIRE
 
-var bodyParser = require("body-parser")
-var express = requrie("express")
+var bodyParser = require("body-parser");
+var express = require("express");
 
 // CREATE EXPRESS APP OBJECT
 
-var app = express()
+var app = express();
 
 // CONFIGURE
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({entended:true}))
-app.use(express.static(__dirname + "/public"))
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static(__dirname + "/public"));
 
 // ROUTES
 
-app.get("/",function(request,response)){
-	response.sendFile('/html/index.html',{root:"./public"})
-}
-
+app.get('/', function(request, response){
+  response.sendFile('/html/index.html', {root : './public'})
+});
 // SET UP SERVER TO LISTEN
 
-var port = 80
+var port = 3000
+app.listen(port, function(){
+  console.log('Server running on port ' + port);
 
-app.listen(port,function(){
-	console.log("Server running on port " + port)
-})
+});
